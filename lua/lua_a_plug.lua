afaa1f93cf09b970828e5d1aae6e455000b5844d
AnthonyK213 <tjjzqwh@163.com>
2021-01-22 15:09:57 +0800

    210122

---
+++ lua/lua_a_plug.lua
@@ -0,0 +1,28 @@
+vim.cmd('packadd paq-nvim')
+local paq = require('paq-nvim').paq
+
+
+paq {'savq/paq-nvim', opt=true}
+-- Visual
+paq {'joshdick/onedark.vim'}
+paq {'vim-airline/vim-airline'}
+-- Tree manager
+paq {'preservim/nerdtree'}
+-- Git utilities
+paq {'tpope/vim-fugitive'}
+paq {'mhinz/vim-signify'}
+paq {'Xuyuanp/nerdtree-git-plugin'}
+-- Utilities
+paq {'AnthonyK213/vim-ipairs'}
+paq {'norcalli/nvim-colorizer.lua'}
+paq {'Yggdroot/indentLine'}
+paq {'tpope/vim-speeddating'}
+paq {'dhruvasagar/vim-table-mode'}
+-- File type support
+paq {'lervag/vimtex'}
+paq {'jceb/vim-orgmode'}
+paq {'plasticboy/vim-markdown'}
+paq {'sophacles/vim-processing'}
+paq {'iamcco/markdown-preview.nvim', hook='cd app & yarn install'}
+--  Completion; LSP
+paq {'neoclide/coc.nvim', branch='release'}
