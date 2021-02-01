1143e549adb5c8fb71db650e808408e141a096a2
AnthonyK213 <tjjzqwh@163.com>
2021-02-02 00:45:03 +0800

    210202

--- lua/package/ui.lua
+++ lua/package/ui.lua
@@ -338,11 +338,7 @@ require'bufferline'.setup {
         sort_by = 'extension'
     }
 }
-vim.api.nvim_set_keymap(
-    'n',
-    '<leader>bb',
-    '<cmd>BufferLinePick<CR>',
-    { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bb', '<cmd>BufferLinePick<CR>', { noremap = true, silent = true })
 
 
 -- nvim-colorizer
