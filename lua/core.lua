5f384044ef4b114d3c374fadbf2aa1582ab79b05
AnthonyK213 <tjjzqwh@163.com>
2021-02-02 12:05:22 +0800

    210202

--- lua/core.lua
+++ lua/core.lua
@@ -1,13 +1,14 @@
 local lib = require("utility/lib")
 local init_src = lib.get_var(vim.g.init_src, 'one')
 
-require('package/paq')
 lib.viml_source('viml/viml_basics')
 lib.viml_source('viml/viml_custom')
 require('internal/var')
 require('internal/map')
 require('internal/cmd')
+require('package/paq')
 require('package/config')
+require('package/inter')
 
 if (init_src == 'nano') then
     vim.o.tgc = true
--- lua/internal/cmd.lua
+++ lua/internal/cmd.lua
@@ -9,17 +9,3 @@ vim.cmd('command! PDF lua require("utility/util").open(vim.fn.expand("%:r")..".p
 vim.cmd('command! -nargs=? CodeRun lua require("utility/util").run_or_compile(<q-args>)')
 -- Git push all
 vim.cmd('command! -nargs=* PushAll lua require("utility/util").git_push_all(<f-args>)')
-
-
--- vim-orgmode
-vim.cmd('command! OrgAgenda :exe ":tabnew" g:org_agenda_path')
--- vimtex
-vim.cmd('augroup completion_nvim_enable_all')
-vim.cmd('autocmd!')
-vim.cmd('au BufEnter * lua require("completion").on_attach()')
-vim.cmd('augroup end')
--- nvim-lspconfig
-vim.cmd('augroup lsp_diagnositic_on_hold')
-vim.cmd('autocmd!')
-vim.cmd('au CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()')
-vim.cmd('augroup end')
