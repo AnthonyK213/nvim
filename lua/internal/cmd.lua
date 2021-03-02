c69e79b09769da457a92e7d3e3b6103e39c8c365
AnthonyK213 <tjjzqwh@163.com>
2021-03-03 01:05:33 +0800

    210303

--- lua/internal/cmd.lua
+++ lua/internal/cmd.lua
@@ -3,23 +3,7 @@ vim.cmd('command! Time :echo strftime("%Y-%m-%d %a %T")')
 -- LaTeX
 vim.cmd('command! PDF lua require("utility/util").open_file(vim.fn.expand("%:r")..".pdf")')
 -- Run or compile
-vim.cmd('command! -nargs=? -complete=customlist,RunCompOpt CodeRun lua require("utility/util").run_or_compile(<q-args>)')
-vim.api.nvim_exec(
-[[
-function! RunCompOpt(A,L,P)
-    let l:ft = &filetype
-    if l:ft == 'c'
-        return ['build', 'check']
-    elseif l:ft == 'rust'
-        return ['build', 'clean', 'check', 'rustc']
-    elseif l:ft == 'tex'
-        return ['biber', 'bibtex']
-    else
-        return ['']
-    endif
-endfunction
-]],
-false)
+vim.cmd('command! -nargs=? -complete=customlist,v:lua.RunCompOpt CodeRun lua require("utility/util").run_or_compile(<q-args>)')
 -- Git push all
 vim.cmd('command! -nargs=* PushAll lua require("utility/util").git_push_all(<f-args>)')
 -- Scroll off
@@ -28,3 +12,17 @@ vim.cmd('autocmd!')
 vim.cmd('au BufEnter * setlocal so=5')
 vim.cmd('au BufEnter *.md setlocal so=999')
 vim.cmd('augroup end')
+
+
+function RunCompOpt()
+    local ft = vim.bo.filetype
+    if ft == 'c' then
+        return {'build', 'check'}
+    elseif ft == 'rust' then
+        return {'build', 'clean', 'check', 'rustc'}
+    elseif ft == 'tex' then
+        return {'biber', 'bibtex'}
+    else
+        return {''}
+    end
+end
