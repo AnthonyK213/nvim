6cb05c3c1e258abe2cfbaeec87cdd4d6d6de7db8
AnthonyK213 <tjjzqwh@163.com>
2021-03-17 14:29:38 +0800

    210317

--- lua/package/ui.lua
+++ lua/package/ui.lua
@@ -12,68 +12,10 @@ vim.cmd('augroup end')
 
 
 -- lualine.nvim
-local mode_alias = {
-    c      = 'C',
-    i      = 'I',
-    ic     = 'I',
-    ix     = 'I',
-    n      = 'N',
-    multi  = 'M',
-    niI    = 'Ĩ',
-    no     = 'N',
-    R      = 'R',
-    Rv     = 'R',
-    s      = 'S',
-    S      = 'S',
-    t      = 'T',
-    v      = 'v',
-    V      = 'V',
-    [''] = 'B',
-}
---- If file is readonly.
-local function file_readonly()
-    if vim.bo.filetype == 'help' then return '' end
-    if vim.bo.readonly == true then return 'RO' end
-    return ''
-end
---- Current mode.
-local function get_current_mode()
-    if mode_alias[vim.fn.mode(1)] ~= nil then
-        return mode_alias[vim.fn.mode(1)]
-    else
-        return '_'
-    end
-end
---- Load status line.
 vim.cmd('packadd lualine.nvim')
-local lualine = require('lualine')
-lualine.options = {
-    theme = 'one'..vim.o.bg,
-    section_separators = nil,
-    component_separators = '│',
-    icons_enabled = false
-}
-lualine.sections = {
-    lualine_a = { get_current_mode },
-    lualine_b = { { 'branch', icons_enabled=true }, { 'diff', colored=true } },
-    lualine_c = { { 'filename', shorten=false, full_path=true }, file_readonly },
-    lualine_x = { { 'diagnostics', sources={ 'nvim_lsp' } }, 'filetype' },
-    lualine_y = { 'encoding', 'fileformat' },
-    lualine_z = { 'progress', 'location' },
-}
-lualine.inactive_sections = {
-    lualine_a = {  },
-    lualine_b = {  },
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {  },
-    lualine_z = {  },
-}
-lualine.extensions = { 'fzf' }
-lualine.status()
-vim.cmd('augroup lualine_color_toggle')
+vim.cmd('augroup lualine_setup')
 vim.cmd('autocmd!')
-vim.cmd('au ColorSchemePre one lua require("lualine").options.theme="one"..vim.o.bg')
+vim.cmd('au ColorScheme one lua require("utility/misc").lualine_setup()')
 vim.cmd('augroup end')
 
 
--- lua/utility/misc.lua
+++ lua/utility/misc.lua
@@ -68,5 +68,70 @@ function misc.indent_guides_color_toggle()
     indent.indent_guides_enable()
 end
 
+-- lualine.nvim setup
+local mode_alias = {
+    c      = 'C',
+    i      = 'I',
+    ic     = 'I',
+    ix     = 'I',
+    n      = 'N',
+    multi  = 'M',
+    niI    = 'Ĩ',
+    no     = 'N',
+    R      = 'R',
+    Rv     = 'R',
+    s      = 'S',
+    S      = 'S',
+    t      = 'T',
+    v      = 'v',
+    V      = 'V',
+    [''] = 'B',
+}
+
+--- If file is readonly.
+local function file_readonly()
+    if vim.bo.filetype == 'help' then return '' end
+    if vim.bo.readonly == true then return 'RO' end
+    return ''
+end
+
+--- Current mode.
+local function get_current_mode()
+    if mode_alias[vim.fn.mode(1)] ~= nil then
+        return mode_alias[vim.fn.mode(1)]
+    else
+        return '_'
+    end
+end
+
+--- lualine setup.
+function misc.lualine_setup()
+    require('lualine').setup {
+        options = {
+            theme = 'one'..vim.o.bg,
+            section_separators = nil,
+            component_separators = nil,
+            icons_enabled = false
+        },
+        sections = {
+            lualine_a = { get_current_mode },
+            lualine_b = { 'branch' },
+            lualine_c = { file_readonly, { 'filename', full_path=true, shorten=false }, 'diff' },
+            lualine_x = { { 'diagnostics', sources={ 'nvim_lsp' } }, 'filetype' },
+            lualine_y = { 'encoding', 'fileformat' },
+            lualine_z = { 'progress', 'location' },
+        },
+        inactive_sections = {
+            lualine_a = { },
+            lualine_b = { },
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = { },
+            lualine_z = { },
+        },
+        extensions = { 'nerdtree' }
+    }
+end
+
 
 return misc
