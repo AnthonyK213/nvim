ce956f930ebb3dfcc530f59e3a5fcc81eab2d747
AnthonyK213 <tjjzqwh@163.com>
2021-03-26 20:33:08 +0800

    210326

--- lua/internal/cmd.lua
+++ lua/internal/cmd.lua
@@ -5,7 +5,7 @@ vim.cmd('command! PDF lua require("utility/util").open_file(vim.fn.expand("%:p:r
 -- Run or compile
 vim.cmd('command! -nargs=? -complete=customlist,v:lua.RUN_CODE_OPTION CodeRun lua require("utility/util").run_or_compile(<q-args>)')
 -- Git push all
-vim.cmd('command! -nargs=* PushAll lua require("utility/util").git_push_all(<f-args>)')
+vim.cmd('command! -nargs=* PushAll lua require("utility/vcs").git_push_all(<f-args>)')
 -- Scroll off
 require("utility/misc").set_au_group(
 'scroll_off',
--- lua/utility/util.lua
+++ lua/utility/util.lua
@@ -269,59 +269,5 @@ function M.run_or_compile(option)
     end
 end
 
---- Git push all
-function M.git_push_all(...)
-    local arg_list = {...}
-    local git_root = lib.get_git_root()
-    local git_branch
-
-    if git_root then
-        git_branch = lib.get_git_branch(git_root)
-    else
-        print("Not a git repository.")
-        return
-    end
-
-    if git_branch then
-        print("Root directory: "..git_root)
-        print("Current branch: "..git_branch)
-        vim.fn.execute('cd '..git_root)
-    else
-        print("Not a valid git repository.")
-        return
-    end
-
-    if #arg_list % 2 == 0 then
-        local m_idx = vim.fn.index(arg_list, "-m") + 1
-        local b_idx = vim.fn.index(arg_list, "-b") + 1
-        local m_arg, b_arg
-
-        if m_idx > 0 and m_idx % 2 == 1 then
-            m_arg = arg_list[m_idx + 1]
-        elseif m_idx == 0 then
-            m_arg = vim.fn.strftime('%y%m%d')
-        else
-            print("Invalid commit argument.")
-            return
-        end
-
-        vim.fn.execute('!git add *')
-        vim.fn.execute('!git commit -m "'..m_arg..'"')
-        print('Commit message: '..m_arg)
-
-        if b_idx > 0 and b_idx % 2 == 1 then
-            b_arg = arg_list[b_idx + 1]
-        elseif b_idx == 0 then
-            b_arg = git_branch
-        else
-            print("Invalid branch argument.")
-        end
-
-        vim.fn.execute('!git push origin '..b_arg, false)
-    else
-        print("Wrong number of arguments is given.")
-    end
-end
-
 
 return M
