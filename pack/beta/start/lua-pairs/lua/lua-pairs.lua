b87a4f0dbc5bd2d51672e62f642daceaa9b5e890
AnthonyK213 <tjjzqwh@163.com>
2021-04-09 13:49:09 +0800

    210409

--- pack/beta/start/lua-pairs/lua/lua-pairs.lua
+++ pack/beta/start/lua-pairs/lua/lua-pairs.lua
@@ -220,7 +220,6 @@ function M.lp_supbs()
         '<C-\\><C-O>'..tostring(res[2] + res[3])..'x')
     elseif back:match('{%s$') and fore:match('^%s}') then
         feed_keys('<C-g>U<Left><Left><C-\\><C-o>4x')
-        return
     else
         feed_keys('<C-\\><C-o>db')
     end
