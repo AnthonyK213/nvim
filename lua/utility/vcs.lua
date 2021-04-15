82fddf1d3945d81c8d1f093dda0e42b84440d54d
AnthonyK213 <tjjzqwh@163.com>
2021-04-15 22:39:19 +0800

    210415

--- lua/utility/vcs.lua
+++ lua/utility/vcs.lua
@@ -3,6 +3,7 @@ local lib = require('utility/lib')
 local uv = vim.loop
 
 
+-- Async?
 local function git_push_async(b_arg)
     Handle_push = uv.spawn('git', {
         args = {'push', 'origin', b_arg}
