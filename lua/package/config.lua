ae872a625294109113d844e312b337b318aec66b
AnthonyK213 <tjjzqwh@163.com>
2021-05-03 14:25:39 +0800

    clean_hunk

--- lua/package/config.lua
+++ lua/package/config.lua
@@ -61,16 +61,14 @@ require('gitsigns').setup {
         noremap = true,
         buffer = true,
 
-        ['n <leader>hj'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-        ['n <leader>hk'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-
+        ['n <leader>hj'] = { expr = true, '<cmd>lua require("gitsigns").next_hunk()<CR>'},
+        ['n <leader>hk'] = { expr = true, '<cmd>lua require("gitsigns").prev_hunk()<CR>'},
+        --['n <leader>hs'] = '<cmd>lua require("gitsigns").stage_hunk()<CR>',
+        --['n <leader>hu'] = '<cmd>lua require("gitsigns").undo_stage_hunk()<CR>',
+        --['n <leader>hr'] = '<cmd>lua require("gitsigns").reset_hunk()<CR>',
+        --['n <leader>hR'] = '<cmd>lua require("gitsigns").reset_buffer()<CR>',
+        ['n <leader>hp'] = '<cmd>lua require("gitsigns").preview_hunk()<CR>',
+        ['n <leader>hb'] = '<cmd>lua require("gitsigns").blame_line()<CR>',
         -- Text objects
         ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
         ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
