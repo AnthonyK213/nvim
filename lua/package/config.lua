61dcc24601b2546d4a00e56f08637b51a9c4c0bc
AnthonyK213 <tjjzqwh@163.com>
2021-05-07 21:18:48 +0800

    clean_gitsigns_config

--- lua/package/config.lua
+++ lua/package/config.lua
@@ -48,28 +48,46 @@ require('neogit').setup {}
 -- gitsigns.nvim
 require('gitsigns').setup {
     signs = {
-        add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-        change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-        delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        add = {
+            hl     = 'GitSignsAdd',
+            text   = '│',
+            numhl  = 'GitSignsAddNr',
+            linehl = 'GitSignsAddLn'
+        },
+        change = {
+            hl     = 'GitSignsChange',
+            text   = '│',
+            numhl  = 'GitSignsChangeNr',
+            linehl = 'GitSignsChangeLn'
+        },
+        delete = {
+            hl     = 'GitSignsDelete',
+            text   = '_',
+            numhl  = 'GitSignsDeleteNr',
+            linehl = 'GitSignsDeleteLn'
+        },
+        topdelete = {
+            hl     = 'GitSignsDelete',
+            text   = '‾',
+            numhl  = 'GitSignsDeleteNr',
+            linehl = 'GitSignsDeleteLn'
+        },
+        changedelete = {
+            hl     = 'GitSignsChange',
+            text   = '~',
+            numhl  = 'GitSignsChangeNr',
+            linehl = 'GitSignsChangeLn'
+        },
     },
     numhl = false,
     linehl = false,
     keymaps = {
-        -- Default keymap options
         noremap = true,
         buffer = true,
-
         ['n <leader>hj'] = '<cmd>lua require("gitsigns").next_hunk()<CR>',
         ['n <leader>hk'] = '<cmd>lua require("gitsigns").prev_hunk()<CR>',
-        --['n <leader>hs'] = '<cmd>lua require("gitsigns").stage_hunk()<CR>',
-        --['n <leader>hu'] = '<cmd>lua require("gitsigns").undo_stage_hunk()<CR>',
-        --['n <leader>hr'] = '<cmd>lua require("gitsigns").reset_hunk()<CR>',
-        --['n <leader>hR'] = '<cmd>lua require("gitsigns").reset_buffer()<CR>',
         ['n <leader>hp'] = '<cmd>lua require("gitsigns").preview_hunk()<CR>',
         ['n <leader>hb'] = '<cmd>lua require("gitsigns").blame_line()<CR>',
-        -- Text objects
         ['o ih'] = ':<C-U>lua require("gitsigns").select_hunk()<CR>',
         ['x ih'] = ':<C-U>lua require("gitsigns").select_hunk()<CR>'
     },
