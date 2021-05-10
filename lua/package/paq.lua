1dd3476f905eeacace64e8473cfe58c83af798f9
AnthonyK213 <tjjzqwh@163.com>
2021-05-10 22:48:31 +0800

    paq->use

--- lua/package/paq.lua
+++ lua/package/paq.lua
@@ -1,34 +1,34 @@
 vim.cmd('packadd paq-nvim')
-local paq = require('paq-nvim').paq
+local use = require('paq-nvim').paq
 
-
-paq {'savq/paq-nvim', opt=true}
+-- Package manager
+use {'savq/paq-nvim', opt = true}
 -- Visual
-paq {'Th3Whit3Wolf/one-nvim', opt=true}
-paq {'hoob3rt/lualine.nvim', opt=true}
-paq {'akinsho/nvim-bufferline.lua', opt=true}
-paq {'glepnir/indent-guides.nvim', opt=true}
-paq {'norcalli/nvim-colorizer.lua', opt=true}
+use {'Th3Whit3Wolf/one-nvim',       opt = true}
+use {'hoob3rt/lualine.nvim',        opt = true}
+use {'akinsho/nvim-bufferline.lua', opt = true}
+use {'glepnir/indent-guides.nvim',  opt = true}
+use {'norcalli/nvim-colorizer.lua', opt = true}
 -- File system
 --paq {'kyazdani42/nvim-tree.lua'}
-paq {'AnthonyK213/nvim-tree.lua'}
-paq {'nvim-telescope/telescope.nvim'}
+use {'AnthonyK213/nvim-tree.lua'}
+use {'nvim-telescope/telescope.nvim'}
 -- Git utilities
-paq {'TimUntersberger/neogit'}
-paq {'lewis6991/gitsigns.nvim'}
+use {'TimUntersberger/neogit'}
+use {'lewis6991/gitsigns.nvim'}
 -- Utilities
-paq {'nvim-lua/popup.nvim'}
-paq {'nvim-lua/plenary.nvim'}
-paq {'tpope/vim-speeddating'}
-paq {'dhruvasagar/vim-table-mode'}
+use {'nvim-lua/popup.nvim'}
+use {'nvim-lua/plenary.nvim'}
+use {'tpope/vim-speeddating'}
+use {'dhruvasagar/vim-table-mode'}
 -- File type support
-paq {'lervag/vimtex'}
-paq {'vimwiki/vimwiki'}
-paq {'plasticboy/vim-markdown'}
-paq {'sophacles/vim-processing'}
-paq {'iamcco/markdown-preview.nvim', run=vim.fn['mkdp#util#install']}
+use {'lervag/vimtex'}
+use {'vimwiki/vimwiki'}
+use {'plasticboy/vim-markdown'}
+use {'sophacles/vim-processing'}
+use {'iamcco/markdown-preview.nvim', run = vim.fn['mkdp#util#install']}
 -- Snippet; Completion; LSP; Treesitter
-paq {'hrsh7th/vim-vsnip'}
-paq {'hrsh7th/nvim-compe'}
-paq {'neovim/nvim-lspconfig'}
-paq {'nvim-treesitter/nvim-treesitter'}
+use {'hrsh7th/vim-vsnip'}
+use {'hrsh7th/nvim-compe'}
+use {'neovim/nvim-lspconfig'}
+use {'nvim-treesitter/nvim-treesitter'}
