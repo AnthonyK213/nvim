71b7546df7db339227880f43da4ba69e3e72a4c5
AnthonyK213 <tjjzqwh@163.com>
2021-05-24 14:56:18 +0800

    210524

--- lua/utility/pub.lua
+++ lua/utility/pub.lua
@@ -29,19 +29,33 @@ M.esc_url = {
     ["\r"] = "\\%20", ["\t"] = "\\%20"
 }
 
-local url_domains = [[.ac.ad.ae.aero.af.ag.ai.al.am.an.ao.aq.ar.arpa.as.asia
-.at.au.aw.ax.az.ba.bb.bd.be.bf.bg.bh.bi.biz.bj.bm.bn.bo.br.bs.bt.bv.bw.by.bz
-.ca.cat.cc.cd.cf.cg.ch.ci.ck.cl.cm.cn.co.com.coop.cr.cs.cu.cv.cx.cy.cz.dd
-.de.dj.dk.dm.do.dz.ec.edu.ee.eg.eh.er.es.et.eu.fi.firm.fj.fk.fm.fo.fr.fx.ga
-.gb.gd.ge.gf.gh.gi.gl.gm.gn.gov.gp.gq.gr.gs.gt.gu.gw.gy.hk.hm.hn.hr.ht.hu
-.id.ie.il.im.in.info.int.io.iq.ir.is.it.je.jm.jo.jobs.jp.ke.kg.kh.ki.km.kn
-.kp.kr.kw.ky.kz.la.lb.lc.li.lk.lr.ls.lt.lu.lv.ly.ma.mc.md.me.mg.mh.mil.mk
-.ml.mm.mn.mo.mobi.mp.mq.mr.ms.mt.mu.museum.mv.mw.mx.my.mz.na.name.nato.nc
-.ne.net.nf.ng.ni.nl.no.nom.np.nr.nt.nu.nz.om.org.pa.pe.pf.pg.ph.pk.pl.pm
-.pn.post.pr.pro.ps.pt.pw.py.qa.re.ro.ru.rw.sa.sb.sc.sd.se.sg.sh.si.sj.sk
-.sl.sm.sn.so.sr.ss.st.store.su.sv.sy.sz.tc.td.tel.tf.tg.th.tj.tk.tl.tm.tn
-.to.tp.tr.travel.tt.tv.tw.tz.ua.ug.uk.um.us.uy.va.vc.ve.vg.vi.vn.vu.web.wf
-.ws.xxx.ye.yt.yu.za.zm.zr.zw]]
+local url_domains =
+[[.ac.ad.ae.aero.af.ag.ai.al.am.an.ao.aq.ar.arpa.as.asia.at.au.aw.ax.az
+.ba.bb.bd.be.bf.bg.bh.bi.biz.bj.bm.bn.bo.br.bs.bt.bv.bw.by.bz
+.ca.cat.cc.cd.cf.cg.ch.ci.ck.cl.cm.cn.co.com.coop.cr.cs.cu.cv.cx.cy.cz
+.dd.de.dj.dk.dm.do.dz
+.ec.edu.ee.eg.eh.er.es.et.eu
+.fi.firm.fj.fk.fm.fo.fr.fx
+.ga.gb.gd.ge.gf.gh.gi.gl.gm.gn.gov.gp.gq.gr.gs.gt.gu.gw.gy
+.hk.hm.hn.hr.ht.hu
+.id.ie.il.im.in.info.int.io.iq.ir.is.it
+.je.jm.jo.jobs.jp
+.ke.kg.kh.ki.km.kn.kp.kr.kw.ky.kz
+.la.lb.lc.li.lk.lr.ls.lt.lu.lv.ly
+.ma.mc.md.me.mg.mh.mil.mk.ml.mm.mn.mo.mobi.mp.mq.mr.ms.mt.mu.museum.mv.mw.mx.my.mz
+.na.name.nato.nc.ne.net.nf.ng.ni.nl.no.nom.np.nr.nt.nu.nz
+.om.org
+.pa.pe.pf.pg.ph.pk.pl.pm.pn.post.pr.pro.ps.pt.pw.py
+.qa
+.re.ro.ru.rw
+.sa.sb.sc.sd.se.sg.sh.si.sj.sk.sl.sm.sn.so.sr.ss.st.store.su.sv.sy.sz
+.tc.td.tel.tf.tg.th.tj.tk.tl.tm.tn.to.tp.tr.travel.tt.tv.tw.tz
+.ua.ug.uk.um.us.uy
+.va.vc.ve.vg.vi.vn.vu
+.web.wf.ws
+.xxx
+.ye.yt.yu
+.za.zm.zr.zw]]
 
 M.url_domain_table = {}
 for domain in url_domains:gmatch('%w+') do
