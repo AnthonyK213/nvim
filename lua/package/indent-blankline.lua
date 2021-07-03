78163e576a66a771f4644e5fa33c00eae40440e0
AnthonyK213 <tjjzqwh@163.com>
2021-07-04 01:41:01 +0800

    210704

--- lua/package/indent-blankline.lua
+++ lua/package/indent-blankline.lua
@@ -1,4 +1,4 @@
-vim.g.indent_blankline_char_list = { '│' }
+vim.g.indent_blankline_char_list = { '¦' }
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_buftype_exclude = {
     'terminal', 'NvimTree'
