c7fc3faae785a71e83a1d5decff0c968bf2cc4ea
AnthonyK213 <tjjzqwh@163.com>
2021-08-20 01:02:13 +0800

    210820

--- lua/utility/util.lua
+++ lua/utility/util.lua
@@ -39,9 +39,9 @@ function M.open_file_or_url(arg)
         arg:match'^%f[%w]%a+://%w[-.%w]*:?%d*/?[%w_.~!*:@&+$/?%%#=-]*$') then
         return
     end
-    Handle = vim.loop.spawn(pub.start, {
-        --args = vim.fn.has('win32') == 1 and {'""', arg} or {arg}
-        args = {arg}
+    local cmd = vim.fn.has('win32') == 1 and "cmd" or pub.start
+    Handle = vim.loop.spawn(cmd, {
+        args = vim.fn.has('win32') == 1 and {'/k', pub.start, arg} or {arg}
     },
     vim.schedule_wrap(function ()
         print(pub.start, arg)
