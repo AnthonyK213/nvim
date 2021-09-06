7f5c817573dd4e618d139c520f940da978a57be2
AnthonyK213 <tjjzqwh@163.com>
2021-09-06 23:37:37 +0800

    210906

--- lua/package/aerial.lua
+++ lua/package/aerial.lua
@@ -1,1 +1,18 @@
+local aerial = require'aerial'
 require('telescope').load_extension('aerial')
+
+local M = {}
+
+function M.aerial_on_attach(client)
+    aerial.on_attach(client)
+    -- Toggle the aerial window with <leader>a
+    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>mt', '<Cmd>AerialToggle!<CR>', {})
+    -- Jump forwards/backwards with '{' and '}'
+    vim.api.nvim_buf_set_keymap(0, 'n', '{', '<Cmd>AerialPrev<CR>', {})
+    vim.api.nvim_buf_set_keymap(0, 'n', '}', '<Cmd>AerialNext<CR>', {})
+    -- Jump up the tree with '[[' or ']]'
+    vim.api.nvim_buf_set_keymap(0, 'n', '[[', '<Cmd>AerialPrevUp<CR>', {})
+    vim.api.nvim_buf_set_keymap(0, 'n', ']]', '<Cmd>AerialNextUp<CR>', {})
+end
+
+return M
--- lua/package/nvim-lspconfig.lua
+++ lua/package/nvim-lspconfig.lua
@@ -4,18 +4,7 @@ local lsp_option = require('core/opt').lsp or {}
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- aerial.nvim
-local aerial = require'aerial'
-local custom_attach = function(client)
-    aerial.on_attach(client)
-    -- Toggle the aerial window with <leader>a
-    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>mt', '<Cmd>AerialToggle!<CR>', {})
-    -- Jump forwards/backwards with '{' and '}'
-    vim.api.nvim_buf_set_keymap(0, 'n', '{', '<cmd>AerialPrev<CR>', {})
-    vim.api.nvim_buf_set_keymap(0, 'n', '}', '<cmd>AerialNext<CR>', {})
-    -- Jump up the tree with '[[' or ']]'
-    vim.api.nvim_buf_set_keymap(0, 'n', '[[', '<cmd>AerialPrevUp<CR>', {})
-    vim.api.nvim_buf_set_keymap(0, 'n', ']]', '<cmd>AerialNextUp<CR>', {})
-end
+local custom_attach = require('package/aerial').aerial_on_attach
 --- clangd
 if lsp_option.clangd then
     lspconfig.clangd.setup {
