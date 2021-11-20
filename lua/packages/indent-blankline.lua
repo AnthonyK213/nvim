15d087ffcd6e89d3418566d412139c973571fc82
AnthonyK213 <tjjzqwh@163.com>
2021-11-20 19:57:37 +0800

    211120

--- lua/packages/indent-blankline.lua
+++ lua/packages/indent-blankline.lua
@@ -1,7 +1,19 @@
 require("indent_blankline").setup {
-    char = "¦",
+    char = "▏",
     use_treesitter = true,
     space_char_blankline = " ",
     show_current_context = true,
+    context_patterns = {
+        'class',
+        'function',
+        'method',
+        '^if',
+        '^while',
+        '^for',
+        '^object',
+        '^table',
+        'block',
+        'arguments',
+    },
     buftype_exclude = {"terminal", 'NvimTree'}
 }
