8565072e46f876ee2fc394e7913008c07fb7dbed
AnthonyK213 <tjjzqwh@163.com>
2021-11-27 09:36:09 +0800

    gps_icons

--- lua/packages/nvim-gps.lua
+++ lua/packages/nvim-gps.lua
@@ -3,8 +3,45 @@ require('nvim-gps').setup({
         ["class-name"] = '[C]',
         ["function-name"] = '[F]',
         ["method-name"] = '[M]',
-        ["container-name"] = '[L]',
-        ["tag-name"] = '<tag> '
+        ["container-name"] = '[T]',
+        ["tag-name"] = '[tag]'
+    },
+    languages = {
+        ["json"] = {
+            icons = {
+                ["array-name"] = '[a]',
+                ["object-name"] = '[O]',
+                ["null-name"] = '[×]',
+                ["boolean-name"] = '[b]',
+                ["number-name"] = '[n]',
+                ["string-name"] = '[s]'
+            }
+        },
+        ["toml"] = {
+            icons = {
+                ["table-name"] = '[T]',
+                ["array-name"] = '[a]',
+                ["boolean-name"] = '[b]',
+                ["date-name"] = '[d]',
+                ["date-time-name"] = '[dt]',
+                ["float-name"] = '[f]',
+                ["inline-table-name"] = '[Ti]',
+                ["integer-name"] = '[i]',
+                ["string-name"] = '[s]',
+                ["time-name"] = '[t]'
+            }
+        },
+        ["yaml"] = {
+            icons = {
+                ["mapping-name"] = '[m]',
+                ["sequence-name"] = '[seq]',
+                ["null-name"] = '[×]',
+                ["boolean-name"] = '[b]',
+                ["integer-name"] = '[i]',
+                ["float-name"] = '[f]',
+                ["string-name"] = '[s]'
+            }
+        },
     },
     separator = ' >> ',
     depth = 0,
