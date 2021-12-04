6e5965c827b07e3fe1dc44d4cfb3c14b6a2e5e95
AnthonyK213 <tjjzqwh@163.com>
2021-12-04 20:19:58 +0800

    indent

--- shell/nvim_nightly_upgrade_linux.sh
+++ shell/nvim_nightly_upgrade_linux.sh
@@ -7,18 +7,18 @@ source="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.
 proxy_default="http://127.0.0.1:10809"
 
 if [ -d "$nvim_dir" ]; then
-    rm -r $nvim_dir
+  rm -r $nvim_dir
 fi
 
 if [ -z "$1" ]; then
-    echo "Using system proxy or no proxy."
-    wget $source -O $archive
+  echo "Using system proxy or no proxy."
+  wget $source -O $archive
 elif [ "$1" = "default" ]; then
-    echo "Using default proxy."
-    curl -L $source -o $archive -x $proxy_default
+  echo "Using default proxy."
+  curl -L $source -o $archive -x $proxy_default
 else
-    echo "Using proxy: $1."
-    curl -L $source -o $archive -x $1
+  echo "Using proxy: $1."
+  curl -L $source -o $archive -x $1
 fi
 
 tar -xf $archive -C "$HOME/.local/bin"
