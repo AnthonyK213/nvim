95233158c6b7e618ade8f2e285f1afd18e0d82ad
AnthonyK213 <tjjzqwh@163.com>
2021-12-25 22:10:25 +0800

    211225

--- lua/packages/neovim-session-manager.lua
+++ lua/packages/neovim-session-manager.lua
@@ -2,16 +2,11 @@ local path = require('plenary.path')
 require('telescope').load_extension('sessions')
 
 require('session_manager').setup {
-    -- The directory where the session files will be saved.
     sessions_dir = path:new(vim.fn.stdpath('data'), 'sessions'),
-    -- The character to which the path separator will be replaced for session files.
     path_replacer = '__',
-    -- The character to which the colon symbol will be replaced for session files.
     colon_replacer = '++',
-    -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
     autoload_mode = require('session_manager.config').AutoloadMode.Disabled,
-    -- Automatically save last session on exit.
     autosave_last_session = true,
-    -- Plugin will not save a session when no writable and listed buffers are opened.
     autosave_ignore_not_normal = true,
+    autosave_only_in_session = false,
 }
