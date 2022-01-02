3e6e8111f501f2c63c88c9be09770d359eba54cf
AnthonyK213 <tjjzqwh@163.com>
2022-01-02 21:36:56 +0800

    upgrade_vimtex

--- lua/packages/vimtex.lua
+++ lua/packages/vimtex.lua
@@ -3,7 +3,5 @@ if (vim.fn.has("win32") == 1) then
     vim.g.vimtex_view_general_viewer = 'SumatraPDF'
     vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 elseif (vim.fn.has("unix") == 1) then
-    vim.g.vimtex_view_general_viewer = 'zathura'
+    vim.g.vimtex_view_method = 'zathura'
 end
-vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
-vim.g.vimtex_compiler_progname = 'nvr'
