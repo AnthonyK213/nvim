6c9c99d130a26fa8662368d90ee971ef9025c831
AnthonyK213 <tjjzqwh@163.com>
2022-01-14 10:24:54 +0800

    key_map

--- lua/packages/nvim-lspconfig.lua
+++ lua/packages/nvim-lspconfig.lua
@@ -1,26 +1,27 @@
 local lspconfig = require('lspconfig')
 local lsp_option = require('core.opt').lsp or {}
-local keymap = vim.api.nvim_buf_set_keymap
+local buf_keymap = vim.api.nvim_buf_set_keymap
 local ntst = { noremap = true, silent = true }
 -- nvim-cmp
 -- Enable LSP snippets.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local custom_attach = function (client)
-    keymap(0, 'n', '<leader>l0', '<Cmd>lua vim.lsp.buf.document_symbol()<CR>',  ntst)
-    keymap(0, 'n', '<leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>',      ntst)
-    keymap(0, 'n', '<leader>ld', '<Cmd>lua vim.lsp.buf.declaration()<CR>',      ntst)
-    keymap(0, 'n', '<leader>lf', '<Cmd>lua vim.lsp.buf.definition()<CR>',       ntst)
-    keymap(0, 'n', '<leader>lh', '<Cmd>lua vim.lsp.buf.signature_help()<CR>',   ntst)
-    keymap(0, 'n', '<leader>li', '<Cmd>lua vim.lsp.buf.implementation()<CR>',   ntst)
-    keymap(0, 'n', '<leader>lm', '<Cmd>lua vim.lsp.buf.formatting_sync()<CR>',  ntst)
-    keymap(0, 'n', '<leader>ln', '<Cmd>lua vim.lsp.buf.rename()<CR>',           ntst)
-    keymap(0, 'n', '<leader>lr', '<Cmd>lua vim.lsp.buf.references()<CR>',       ntst)
-    keymap(0, 'n', '<leader>lt', '<Cmd>lua vim.lsp.buf.type_definition()<CR>',  ntst)
-    keymap(0, 'n', '<leader>lw', '<Cmd>lua vim.lsp.buf.workspace_symbol()<CR>', ntst)
-    keymap(0, 'n', '<leader>l[', '<Cmd>lua vim.diagnostic.goto_prev()<CR>',     ntst)
-    keymap(0, 'n', '<leader>l]', '<Cmd>lua vim.diagnostic.goto_next()<CR>',     ntst)
-    keymap(0, 'n', '<M-K>', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', ntst)
+    buf_keymap(0, 'n', '<leader>l0', '<Cmd>lua vim.lsp.buf.document_symbol()<CR>',  ntst)
+    buf_keymap(0, 'n', '<leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>',      ntst)
+    buf_keymap(0, 'n', '<leader>ld', '<Cmd>lua vim.lsp.buf.declaration()<CR>',      ntst)
+    buf_keymap(0, 'n', '<leader>lf', '<Cmd>lua vim.lsp.buf.definition()<CR>',       ntst)
+    buf_keymap(0, 'n', '<F12>',      '<Cmd>lua vim.lsp.buf.definition()<CR>',       ntst)
+    buf_keymap(0, 'n', '<leader>lh', '<Cmd>lua vim.lsp.buf.signature_help()<CR>',   ntst)
+    buf_keymap(0, 'n', '<leader>li', '<Cmd>lua vim.lsp.buf.implementation()<CR>',   ntst)
+    buf_keymap(0, 'n', '<leader>lm', '<Cmd>lua vim.lsp.buf.formatting_sync()<CR>',  ntst)
+    buf_keymap(0, 'n', '<leader>ln', '<Cmd>lua vim.lsp.buf.rename()<CR>',           ntst)
+    buf_keymap(0, 'n', '<leader>lr', '<Cmd>lua vim.lsp.buf.references()<CR>',       ntst)
+    buf_keymap(0, 'n', '<leader>lt', '<Cmd>lua vim.lsp.buf.type_definition()<CR>',  ntst)
+    buf_keymap(0, 'n', '<leader>lw', '<Cmd>lua vim.lsp.buf.workspace_symbol()<CR>', ntst)
+    buf_keymap(0, 'n', '<leader>l[', '<Cmd>lua vim.diagnostic.goto_prev()<CR>',     ntst)
+    buf_keymap(0, 'n', '<leader>l]', '<Cmd>lua vim.diagnostic.goto_next()<CR>',     ntst)
+    buf_keymap(0, 'n', '<M-K>', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', ntst)
     -- aerial.nvim
     require('aerial').on_attach(client)
 end
