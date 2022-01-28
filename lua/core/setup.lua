e342d21eab7821419787f7b46527c4a042480ffd
AnthonyK213 <tjjzqwh@163.com>
2022-01-28 18:16:47 +0800

    kbd

--- lua/core/setup.lua
+++ lua/core/setup.lua
@@ -3,7 +3,11 @@ require('utility.lib').vim_source('viml/basics')
 
 -- Internal settings
 require('internal.var')
-require('internal.map')
+if vim.fn.has("nvim-0.7") == 1 then
+    require('internal.map_n')
+else
+    require('internal.map')
+end
 require('internal.cmd')
 
 -- Package configurations
