f8112b9ffabfd88fcac7a9ffd4d3d4bb3667b625
AnthonyK213 <tjjzqwh@163.com>
2022-01-30 12:04:03 +0800

    220130

--- lua/internal/map.lua
+++ lua/internal/map.lua
@@ -1,9 +1,21 @@
 local has_new_keymap = vim.fn.has('nvim-0.7') == 1
-
 local kbd = has_new_keymap and vim.keymap.set or vim.api.nvim_set_keymap
 local nt = { noremap = true }
 local ntst = { noremap = true, silent = true }
 local ntstet = { noremap = true, silent = true, expr = true }
+local web_list = {
+    b = "https://www.baidu.com/s?wd=",
+    g = "https://www.google.com/search?q=",
+    h = "https://github.com/search?q=",
+    y = "https://dict.youdao.com/w/eng/"
+}
+local srd_md = {
+    P = '`',
+    I = '*',
+    B = '**',
+    M = '***',
+    U = '<u>',
+}
 
 local get_mode = function ()
     local m = vim.fn.mode()
@@ -79,7 +91,7 @@ kbd('i', '<C-K>', '<C-\\><C-O>D',        ntst)
 kbd('i', '<C-F>', [[col('.') >= col('$') ? "<C-\><C-O>+" : g:const_dir_r]],     ntstet)
 kbd('i', '<C-B>', [[col('.') == 1 ? "<C-\><C-O>-<C-\><C-O>$" : g:const_dir_l]], ntstet)
 kbd('i', '<M-d>', '<C-\\><C-O>dw', ntst)
-for key, val in pairs({n='j', p='k'}) do
+for key, val in pairs({ n = 'j', p = 'k' }) do
     kbd('n', '<C-'..key..'>', 'g'..val,            ntst)
     kbd('v', '<C-'..key..'>', 'g'..val,            ntst)
     kbd('i', '<C-'..key..'>', '<C-\\><C-O>g'..val, ntst)
@@ -176,12 +188,6 @@ if has_new_keymap then
     -- Echo git status.
     kbd('n', '<leader>gs', ':!git status<CR>', ntst)
     -- Search cword in web browser.
-    local web_list = {
-        b = "https://www.baidu.com/s?wd=",
-        g = "https://www.google.com/search?q=",
-        h = "https://github.com/search?q=",
-        y = "https://dict.youdao.com/w/eng/"
-    }
     for key, val in pairs(web_list) do
         kbd({'n', 'v'}, '<leader>h'..key, function ()
             local lib = require('utility.lib')
@@ -214,7 +220,7 @@ if has_new_keymap then
     kbd('n', '<leader>sc', function ()
         require('utility.srd').srd_sub()
     end, ntst)
-    for key, val in pairs({P='`', I='*', B='**', M='***', U='<u>'}) do
+    for key, val in pairs(srd_md) do
         kbd('n', '<M-'..key..'>', function ()
             require("utility.srd").srd_add('n', val)
         end,  ntst)
@@ -233,4 +239,81 @@ if has_new_keymap then
         require('utility.vis').show_hl_captures()
     end, ntst)
 else
+    -- Mouse toggle.
+    kbd('n', '<F2>', '<Cmd>call usr#misc#mouse_toggle()<CR>',            ntst)
+    kbd('v', '<F2>', ':<C-U>call usr#misc#mouse_toggle()<CR>',           ntst)
+    kbd('i', '<F2>', '<C-\\><C-O><Cmd>call usr#misc#mouse_toggle()<CR>', ntst)
+    kbd('t', '<F2>', '<C-\\><C-N><Cmd>call usr#misc#mouse_toggle()<CR>', ntst)
+    -- Run code.
+    kbd('n', '<F5>', '<Cmd>lua require("utility.comp").run_or_compile("")<CR>', ntst)
+    -- Show document.
+    kbd('n', 'K', '<Cmd>lua require("utility.util").show_doc()<CR>', ntst)
+    -- Search visual selection.
+    kbd('v', '*', [[<ESC>/\V<C-r>=v:lua.require("utility.lib").get_visual_selection()<CR><CR>]], ntst)
+    -- Buffer.
+    kbd('n', '<leader>bc', '<Cmd>cd %:p:h<CR>:pwd<CR>', nt)
+    kbd('n', '<leader>bd',
+    [[index(['help', 'terminal', 'nofile', 'quickfix'], &buftype) >= 0 ]]
+    ..[[|| len(getbufinfo({'buflisted':1})) <= 2 ?]]
+    ..[[":bd<CR>" : ":bp|bd#<CR>"]], ntstet)
+    -- Background toggle.
+    kbd('n', '<leader>bg', '<Cmd>call usr#misc#bg_toggle()<CR>', ntst)
+    -- Open opt file.
+    kbd('n', '<M-,>', '<Cmd>call usr#misc#open_opt()<CR>', ntst)
+    -- Explorer.
+    kbd('n', '<leader>oe', '<Cmd>lua require("utility.util").sys_open(vim.fn.expand("%:p:h"))<CR>', ntst)
+    -- Terminal.
+    kbd('n', '<leader>ot', '<Cmd>lua require("utility.util").terminal()<CR>i', ntst)
+    -- Open file of current buffer with system default browser.
+    kbd('n', '<leader>ob', '<Cmd>lua require("utility.util").sys_open(vim.fn.expand("%:p"))<CR>', ntst)
+    -- Open path or url under the cursor or in the selection.
+    kbd('n', '<leader>ou',
+    [[<Cmd>lua local util = require("utility.util") ]]..
+    [[util.sys_open(util.match_path_or_url_under_cursor(), true)<CR>]], ntst)
+    -- Evaluate formula surrounded by `.
+    kbd('n', '<leader>ev', '<Cmd>lua require("utility.eval").lua_eval()<CR>',  ntst)
+    kbd('n', '<leader>el', '<Cmd>lua require("utility.eval").lisp_eval()<CR>', ntst)
+    -- Append day of week after the date.
+    kbd('n', '<leader>dd', ':lua require("utility.gtd").append_day_from_date()<CR>', ntst)
+    -- Print TODO list.
+    kbd("n", "<leader>dt", '<Cmd>lua require("utility.gtd").print_todo_list()<CR>', ntst)
+    -- Hanzi count.
+    kbd('n', '<leader>cc', [[<Cmd>lua ]]
+    ..[[local txt = vim.api.nvim_buf_get_lines(0, 0, -1, false) ]]
+    ..[[require("utility.note").hanzi_count(txt)<CR>]],  ntst)
+    kbd('v', '<leader>cc', [[:<C-u>lua]]
+    ..[[local txt = require('utility.lib').get_visual_selection(true) ]]
+    ..[[require("utility.note").hanzi_count("v")<CR>]], ntst)
+    -- List bullets.
+    kbd('i', '<M-CR>', '<C-\\><C-O>:lua require("utility.note").md_insert_bullet()<CR>',  ntst)
+    kbd('n', '<leader>ml', ':lua require("utility.note").md_sort_num_bullet()<CR>', ntst)
+    -- Echo git status.
+    kbd('n', '<leader>gs', ':!git status<CR>', ntst)
+    -- Search cword in web browser.
+    for key, val in pairs(web_list) do
+        kbd('n', '<leader>h'..key, [[<Cmd>lua ]]
+        ..[[local lib = require('utility.lib') ]]
+        ..[[local txt = lib.encode_url(lib.get_word()) ]]
+        ..[[require('utility.util').sys_open("]]..val..'"..txt)<CR>', ntst)
+        kbd('v', '<leader>h'..key, [[:<C-U>lua ]]
+        ..[[local lib = require('utility.lib') ]]
+        ..[[local txt = lib.encode_url(lib.get_visual_selection(true)) ]]
+        ..[[require('utility.util').sys_open("]]..val..'"..txt)<CR>', ntst)
+    end
+    -- Surround
+    kbd('n', '<leader>sa', '<Cmd>lua require("utility.srd").srd_add("n")<CR>',  ntst)
+    kbd('v', '<leader>sa', ':<C-U>lua require("utility.srd").srd_add("v")<CR>', ntst)
+    kbd('n', '<leader>sd', '<Cmd>lua require("utility.srd").srd_sub("")<CR>',   ntst)
+    kbd('n', '<leader>sc', '<Cmd>lua require("utility.srd").srd_sub()<CR>',     ntst)
+    for key, val in pairs(srd_md) do
+        kbd('n', '<M-'..key..'>', '<Cmd>lua require("utility.srd").srd_add("n","'..val..'")<CR>',  ntst)
+        kbd('v', '<M-'..key..'>', ':<C-U>lua require("utility.srd").srd_add("v","'..val..'")<CR>', ntst)
+    end
+    -- Comment
+    kbd("n", "<leader>kc", '<Cmd>lua require("utility.cmt").cmt_add_norm()<CR>', ntst)
+    kbd("n", "<leader>ku", '<Cmd>lua require("utility.cmt").cmt_del_norm()<CR>', ntst)
+    kbd("v", "<leader>kc", ':<C-U>lua require("utility.cmt").cmt_add_vis()<CR>', ntst)
+    kbd("v", "<leader>ku", ':<C-U>lua require("utility.cmt").cmt_del_vis()<CR>', ntst)
+    -- Show highlight information.
+    kbd("n", "<leader>vs", '<Cmd>lua require("utility.vis").show_hl_captures()<CR>', ntst)
 end
