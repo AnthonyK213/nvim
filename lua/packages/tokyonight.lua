6c2b28cc1327c83af1a499bde5bd049896aa7e1f
AnthonyK213 <tjjzqwh@163.com>
2022-03-14 13:28:52 +0800

    fuck

--- lua/packages/tokyonight.lua
+++ lua/packages/tokyonight.lua
@@ -2,11 +2,13 @@ vim.cmd('packadd tokyonight.nvim')
 
 
 vim.g.tokyonight_style = core_opt.tui.style or 'storm'
+vim.g.tokyonight_transparent = core_opt.tui.transparent
 vim.g.tokyonight_italic_keywords = false
 vim.g.tokyonight_sidebars = {
     "help", "qf", "terminal",
     "aerial", "packer",
 }
+vim.g.tokyonight_colors = {}
 
 
 vim.cmd[[colorscheme tokyonight]]
