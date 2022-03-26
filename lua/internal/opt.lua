52a365f27440835baa61924fa0eb6dd2d48244b1
AnthonyK213 <tjjzqwh@163.com>
2022-03-26 22:43:47 +0800

    220326

--- lua/internal/opt.lua
+++ lua/internal/opt.lua
@@ -98,7 +98,7 @@ vim.g.python3_host_prog = opt.dep.py3
 vim.g.markdown_fenced_languages = {
     "c", "cpp", "cs", "rust", "lua", "vim", "python", "lisp", "tex",
     "javascript", "typescript", "json", "sh", "ps1", "dosbatch",
-    "ruby", "java", "go", "html", "xml", "perl"
+    "cmake", "ruby", "java", "go", "perl", "html", "xml", "toml", "yaml"
 }
 
 -- Directional operation which won't break the history.
