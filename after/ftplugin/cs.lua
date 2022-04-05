de2f9d3eb3d7d85fc1c10478c6c8fde2d4211a26
AnthonyK213 <tjjzqwh@163.com>
2022-04-05 15:05:28 +0800

    fix

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -4,7 +4,7 @@ local buf_handle = vim.api.nvim_get_current_buf()
 
 vim.defer_fn(function ()
     new_keymap("i", '/', function (fallback)
-        if lib.get_context.b:match('^%s*//$') then
+        if lib.get_context().b:match('^%s*//$') then
             lib.feedkeys('/ <summary>\n\n</summary><Up> ', 'n', true)
         else
             fallback()
