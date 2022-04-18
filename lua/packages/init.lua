b98e00c51c3ec9b8846e1d4e04b8b4c1640c7bb5
AnthonyK213 <tjjzqwh@163.com>
2022-04-19 00:56:32 +0800

    220419

--- lua/packages/init.lua
+++ lua/packages/init.lua
@@ -123,7 +123,6 @@ require('packer').startup(function (use)
         };
         {
             'nvim-treesitter/nvim-treesitter',
-            commit = "21ac88b9551072f49ba994f461f78794af43a5aa",
             config = function () require('packages.nvim-treesitter') end
         };
         {
