a14fbe949d7b782c2789736101aaaa1c6d3b51df
AnthonyK213 <tjjzqwh@163.com>
2022-04-27 13:11:27 +0800

    220427

--- lua/utility/note.lua
+++ lua/utility/note.lua
@@ -44,7 +44,8 @@ local function md_check_line(lnum)
     local _, indent = lstr:find('^%s*', 1, false)
     local detect = 0
     if lstr:match('^%s*$')
-        and Syntax:new(lnum, 0):match[[\v(TSLiteral|Code|textSnip)]] then
+        and Syntax:new(lnum, 0)
+        :match[[\v(TSLiteral|markdownHighlight|markdownCode|textSnip)]] then
         indent = 1000
     end
     local bullet
