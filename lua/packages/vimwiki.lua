985df1ca3c3658155301528c9398e370a01a6479
AnthonyK213 <tjjzqwh@163.com>
2022-05-07 13:35:19 +0800

    prev_mkd

--- lua/packages/vimwiki.lua
+++ lua/packages/vimwiki.lua
@@ -2,9 +2,9 @@ vim.g.vimwiki_list = {{
     path = vim.fn.expand(_my_core_opt.path.cloud.."/Notes/"),
     path_html = vim.fn.expand(_my_core_opt.path.cloud.."/Notes/html/"),
     syntax = 'markdown',
-    ext = '.wiki'
+    ext = '.markdown'
 }}
 vim.g.vimwiki_folding = 'syntax'
 vim.g.vimwiki_ext2syntax = {
-    ['.wiki'] = 'markdown'
+    ['.markdown'] = 'markdown'
 }
