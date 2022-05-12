39b069b29ee17a6ceb0c141b90a8950432a07ed7
AnthonyK213 <tjjzqwh@163.com>
2022-05-12 20:48:04 +0800

    220512

--- lua/packages/nvim-lspconfig.lua
+++ lua/packages/nvim-lspconfig.lua
@@ -10,26 +10,26 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local kbd = vim.keymap.set
 local custom_attach = function (client, bufnr)
     local ntst = { noremap = true, silent = true, buffer = bufnr }
+    kbd('n', '<F12>',      function () vim.lsp.buf.definition() end,       ntst)
+    kbd('n', 'K',          function () vim.lsp.buf.hover() end,            ntst)
     kbd('n', '<leader>l0', function () vim.lsp.buf.document_symbol() end,  ntst)
     kbd('n', '<leader>la', function () vim.lsp.buf.code_action() end,      ntst)
     kbd('n', '<leader>ld', function () vim.lsp.buf.declaration() end,      ntst)
     kbd('n', '<leader>lf', function () vim.lsp.buf.definition() end,       ntst)
-    kbd('n', '<F12>',      function () vim.lsp.buf.definition() end,       ntst)
     kbd('n', '<leader>lh', function () vim.lsp.buf.signature_help() end,   ntst)
     kbd('n', '<leader>li', function () vim.lsp.buf.implementation() end,   ntst)
+    kbd('n', '<leader>lk', function ()
+        vim.diagnostic.open_float { border = "rounded" }
+    end, ntst)
+    kbd('n', '<leader>lm', function ()
+        vim.lsp.buf.format { async = false }
+    end, ntst)
     kbd('n', '<leader>ln', function () vim.lsp.buf.rename() end,           ntst)
     kbd('n', '<leader>lr', function () vim.lsp.buf.references() end,       ntst)
     kbd('n', '<leader>lt', function () vim.lsp.buf.type_definition() end,  ntst)
     kbd('n', '<leader>lw', function () vim.lsp.buf.workspace_symbol() end, ntst)
     kbd('n', '<leader>l[', function () vim.diagnostic.goto_prev() end,     ntst)
     kbd('n', '<leader>l]', function () vim.diagnostic.goto_next() end,     ntst)
-    kbd('n', 'K',          function () vim.lsp.buf.hover() end,            ntst)
-    kbd('n', '<leader>lm', function ()
-        vim.lsp.buf.format { async = false }
-    end, ntst)
-    kbd('n', '<M-K>', function ()
-        vim.diagnostic.open_float { border = "rounded" }
-    end, ntst)
     -- aerial.nvim
     require('aerial').on_attach(client, bufnr)
 end
