2fe73790fc9d4d2e9441b6e042f33707a7e46843
AnthonyK213 <tjjzqwh@163.com>
2022-08-05 22:26:52 +0800

    fix

--- lua/utility/srd.lua
+++ lua/utility/srd.lua
@@ -100,7 +100,7 @@ function M.srd_add(mode, pair_a)
         elseif mode == "v" then
             local sl, sc = unpack(api.nvim_buf_get_mark(0, "<"))
             local el, ec = unpack(api.nvim_buf_get_mark(0, ">"))
-            local d = #lib.str_sub(api.nvim_buf_get_text(0, el - 1, ec, el - 1, -1, {})[1], 1)
+            local d = #lib.str_sub(api.nvim_buf_get_text(0, el - 1, ec, el - 1, -1, {})[1], 1, 1)
             api.nvim_buf_set_text(0, el - 1, ec + d, el - 1, ec + d, { p_b })
             api.nvim_buf_set_text(0, sl - 1, sc, sl - 1, sc, { p_a })
         end
