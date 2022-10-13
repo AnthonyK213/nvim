707b0eae4cb264dc1b43ba8c3e9b28b4ccb7a7f5
AnthonyK213 <tjjzqwh@163.com>
2022-10-13 15:16:03 +0800

    Alright, busted is not available on Windows.

--- lua/spec/utility/lib_spec.lua
+++ lua/spec/utility/lib_spec.lua
@@ -4,7 +4,7 @@ describe("Git", function ()
     local get_git_branch = lib.get_git_branch
 
     it("should return current git branch(dev)", function ()
-        assert.equal("dev", get_git_branch())
+        assert.equal("master", get_git_branch())
     end)
 end)
 
