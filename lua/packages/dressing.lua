d6d96af207404653f5215b0be5f2077a22763f78
AnthonyK213 <tjjzqwh@163.com>
2022-10-25 14:02:02 +0800

    fix: selection box border style.

--- lua/packages/dressing.lua
+++ lua/packages/dressing.lua
@@ -1,3 +1,22 @@
+local border_style = _my_core_opt.tui.border
+local border_styles = {
+    single = {
+        prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+        results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
+        preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+    },
+    double = {
+        prompt = { "═", "║", " ", "║", "╔", "╗", "║", "║" },
+        results = { "═", "║", "═", "║", "╠", "╣", "╝", "╚" },
+        preview = { "═", "║", "═", "║", "╔", "╗", "╝", "╚" },
+    },
+    rounded = {
+        prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+        results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+        preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    },
+}
+
 require("dressing").setup {
     input = {
         default_prompt = "> ",
@@ -12,6 +31,10 @@ require("dressing").setup {
     select = {
         backend = { "telescope" },
         format_item_override = {},
+        telescope = require("telescope.themes").get_dropdown {
+            border = border_style ~= "none",
+            borderchars = border_styles[border_style] or border_styles["rounded"],
+        },
         get_config = nil,
     },
 }
