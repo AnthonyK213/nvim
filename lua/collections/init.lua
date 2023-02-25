39cbcea44c19407551f363463d8d1ce9c42e4759
AnthonyK213 <tjjzqwh@163.com>
2023-02-25 22:42:14 +0800

    230225

--- lua/collections/init.lua
+++ lua/collections/init.lua
@@ -1,9 +1,39 @@
-return {
-    Deque = require("collections.deque");
-    HashSet = require("collections.hash_set");
-    Iterator = require("collections.iter");
-    LinkedList = require("collections.linked_list").LinkedList;
-    LinkedListNode = require("collections.linked_list").LinkedListNode;
-    List = require("collections.list");
-    Stack = require("collections.stack");
+local files = {
+    "deque",
+    "hash_set",
+    "iter",
+    "linked_list",
+    "list",
+    "stack",
+    "util",
 }
+
+---@class collection
+---@field reload fun():collection
+local collection = {
+    Deque = require("collections.deque"),
+    HashSet = require("collections.hash_set"),
+    Iterator = require("collections.iter"),
+    LinkedList = require("collections.linked_list").LinkedList,
+    LinkedListNode = require("collections.linked_list").LinkedListNode,
+    List = require("collections.list"),
+    Stack = require("collections.stack"),
+}
+
+setmetatable(collection, {
+    __index = {
+        _VERSION = "0.1.0",
+        _DESCRIPTION = [[Contains interfaces and classes that define various ]]
+        .. [[collections of objects, such as lists, queues and hashsets.]],
+        reload = function()
+            for _, f in ipairs(files) do
+                if package.loaded["collections." .. f] then
+                    package.loaded["collections." .. f] = nil
+                end
+            end
+            return require("utility.test").r("collections")
+        end
+    }
+})
+
+return collection
