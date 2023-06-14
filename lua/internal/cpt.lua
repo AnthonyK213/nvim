33b7ca67cb9f84ed0cde4a13b67fb64dc12bfa95
AnthonyK213 <tjjzqwh@163.com>
2023-06-14 14:46:50 +0800

    230614

--- lua/internal/cpt.lua
+++ lua/internal/cpt.lua
@@ -1,3 +1,11 @@
+local old_version = false
+
 if not vim.uv then
     vim.uv = vim.loop
+    old_version = true
+end
+
+if old_version then
+    vim.notify_once("The current version may have some deprecated features, time to upgrade!",
+        vim.log.levels.WARN)
 end
