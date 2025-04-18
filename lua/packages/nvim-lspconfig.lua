eddfe5fcd73339f8587129edf31e299bb8813f94
AnthonyK213 <tjjzqwh@outlook.com>
2025-04-19 00:07:47 +0800

    update: lsp config

--- lua/packages/nvim-lspconfig.lua
+++ lua/packages/nvim-lspconfig.lua
@@ -1,57 +1,65 @@
-local lsp_option = _my_core_opt.lsp or {}
 local lspconfig = require("lspconfig")
-local kbd = vim.keymap.set
+
 local float_opts = {
-  border = _my_core_opt.tui.border,
+  border = _G._my_core_opt.tui.border,
   max_width = 80,
 }
 
 -- nvim-cmp
--- Enable LSP snippets.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Attaches.
-local custom_attach = function(client, bufnr)
-  local _o = { noremap = true, silent = true, buffer = bufnr }
-  local lsp_fmt = function() vim.lsp.buf.format { async = false } end
-  -- VSCode
+local function custom_attach(client, bufnr)
   local builtin = require("telescope.builtin")
-  local lsp_ref = function() builtin.lsp_references { show_line = false } end
-  if client.name == "omnisharp" then
-    kbd("n", "<F12>", require("omnisharp_extended").telescope_lsp_definitions, _o)
-  else
-    kbd("n", "<F12>", builtin.lsp_definitions, _o)
-  end
-  kbd("n", "<F2>", vim.lsp.buf.rename, _o)
-  kbd("n", "<S-F12>", lsp_ref, _o)
-  kbd("n", "<F24>", lsp_ref, _o)
-  kbd("n", "<C-F12>", builtin.lsp_implementations, _o)
-  kbd("n", "<F36>", builtin.lsp_implementations, _o)
-  kbd("n", "<M-F>", lsp_fmt, _o)
-  -- Neovim LSP
-  kbd("n", "K", function() vim.lsp.buf.hover(float_opts) end, _o)
-  kbd("n", "<leader>l0", vim.lsp.buf.document_symbol, _o)
-  kbd("n", "<leader>la", vim.lsp.buf.code_action, _o)
-  kbd("n", "<leader>ld", vim.lsp.buf.declaration, _o)
-  kbd("n", "<leader>lf", vim.lsp.buf.definition, _o)
-  kbd("n", "<leader>lh", vim.lsp.buf.signature_help, _o)
-  kbd("n", "<leader>li", vim.lsp.buf.implementation, _o)
-  kbd("n", "<leader>lm", lsp_fmt, _o)
-  kbd("n", "<leader>ln", vim.lsp.buf.rename, _o)
-  kbd("n", "<leader>lr", vim.lsp.buf.references, _o)
-  kbd("n", "<leader>lt", vim.lsp.buf.type_definition, _o)
-  kbd("n", "<leader>lw", vim.lsp.buf.workspace_symbol, _o)
-  kbd("n", "<leader>lk", function() vim.diagnostic.open_float(float_opts) end, _o)
+  local kbd = vim.keymap.set
+  local kbd_opts = {
+    noremap = true,
+    silent = true,
+    buffer = bufnr
+  }
 
-  kbd("n", "<leader>l[", function() vim.diagnostic.jump { count = -1, float = float_opts } end, _o)
-  kbd("n", "<leader>l]", function() vim.diagnostic.jump { count = 1, float = float_opts } end, _o)
+  local lsp_fmt = function()
+    vim.lsp.buf.format { async = false }
+  end
+
+  local lsp_ref = function()
+    builtin.lsp_references { show_line = false }
+  end
+
+  if client.name == "omnisharp" then
+    kbd("n", "<F12>", require("omnisharp_extended").telescope_lsp_definitions, kbd_opts)
+  else
+    kbd("n", "<F12>", builtin.lsp_definitions, kbd_opts)
+  end
+  kbd("n", "<F2>", vim.lsp.buf.rename, kbd_opts)
+  kbd("n", "<S-F12>", lsp_ref, kbd_opts)
+  kbd("n", "<F24>", lsp_ref, kbd_opts)
+  kbd("n", "<C-F12>", builtin.lsp_implementations, kbd_opts)
+  kbd("n", "<F36>", builtin.lsp_implementations, kbd_opts)
+  kbd("n", "<M-F>", lsp_fmt, kbd_opts)
+
+  kbd("n", "K", function() vim.lsp.buf.hover(float_opts) end, kbd_opts)
+  kbd("n", "<leader>l0", vim.lsp.buf.document_symbol, kbd_opts)
+  kbd("n", "<leader>la", vim.lsp.buf.code_action, kbd_opts)
+  kbd("n", "<leader>ld", vim.lsp.buf.declaration, kbd_opts)
+  kbd("n", "<leader>lf", vim.lsp.buf.definition, kbd_opts)
+  kbd("n", "<leader>lh", vim.lsp.buf.signature_help, kbd_opts)
+  kbd("n", "<leader>li", vim.lsp.buf.implementation, kbd_opts)
+  kbd("n", "<leader>lm", lsp_fmt, kbd_opts)
+  kbd("n", "<leader>ln", vim.lsp.buf.rename, kbd_opts)
+  kbd("n", "<leader>lr", vim.lsp.buf.references, kbd_opts)
+  kbd("n", "<leader>lt", vim.lsp.buf.type_definition, kbd_opts)
+  kbd("n", "<leader>lw", vim.lsp.buf.workspace_symbol, kbd_opts)
+  kbd("n", "<leader>lk", function() vim.diagnostic.open_float(float_opts) end, kbd_opts)
+  kbd("n", "<leader>l[", function() vim.diagnostic.jump { count = -1, float = float_opts } end, kbd_opts)
+  kbd("n", "<leader>l]", function() vim.diagnostic.jump { count = 1, float = float_opts } end, kbd_opts)
 end
 
--- LSP options.
+-- Modify or overwrite the config of a server.
 local server_settings = {
-  clangd = function(o, s)
-    o.on_new_config = function(new_config, new_cwd)
+  clangd = function(o, _)
+    o.on_new_config = function(new_config, _)
       local status, cmake = pcall(require, "cmake-tools")
       if status then
         cmake.clangd_on_new_config(new_config)
@@ -73,49 +81,67 @@ local server_settings = {
 }
 
 ---Setup servers via nvim-lspconfig.
----@param server string Name of the language server.
+---@param name string Name of the language server.
 ---@param config boolean|table<string, any> Server configuration from `nvimrc`.
-local function setup_server(server, config)
-  config = config or false
-  if (type(config) == "boolean" and config)
-      or (type(config) == "table" and config.load) then
-    local opts = {
-      capabilities = capabilities,
-      on_attach = custom_attach
-    }
-    -- Disable semantic tokens.
-    if type(config) == "table" and config.disable_semantic_tokens then
-      opts.on_attach = function(client, bufnr)
-        client.server_capabilities.semanticTokensProvider = nil
-        custom_attach(client, bufnr)
-      end
-    end
-    -- Merge custom LSP configurations.
-    local option_settings
-    if type(config) == "table" and type(config.settings) == "table" then
-      option_settings = config.settings
-    end
-    local server_setting = server_settings[server]
-    if type(server_setting) == "function" then
-      server_setting(opts, option_settings)
-    else
-      local s = option_settings or server_setting
-      if s then
-        opts.settings = s
-      end
-    end
-    lspconfig[server].setup(opts)
+local function setup_server(name, config)
+  local config_table
+
+  if type(config) == "boolean" then
+    config_table = { load = config }
+  elseif type(config) == "table" then
+    config_table = config
+  else
+    return
   end
+
+  if not config_table.load then
+    return
+  end
+
+  ---@type vim.lsp.Config
+  local cfg = {
+    capabilities = capabilities,
+    on_attach = custom_attach
+  }
+
+  -- Disable semantic tokens.
+  if config_table.disable_semantic_tokens then
+    cfg.on_attach = function(client, bufnr)
+      client.server_capabilities.semanticTokensProvider = nil
+      custom_attach(client, bufnr)
+    end
+  end
+
+  -- Merge custom LSP configurations.
+  local option_settings
+  if type(config_table.settings) == "table" then
+    option_settings = config_table.settings
+  end
+  local server_setting = server_settings[name]
+  if type(server_setting) == "function" then
+    server_setting(cfg, option_settings)
+  else
+    local s = option_settings or server_setting
+    if s then
+      cfg.settings = s
+    end
+  end
+
+  -- Enable the server.
+  lspconfig[name].setup(cfg)
 end
 
 -- Setup servers.
-for server, option in pairs(lsp_option) do setup_server(server, option) end
+for name, config in pairs(_G._my_core_opt.lsp or {}) do
+  setup_server(name, config)
+end
 
 -- Diagnostics
 vim.diagnostic.config {
-  virtual_text = true,
-  signs = true,
-  underline = true,
+  virtual_text     = true,
+  signs            = true,
+  underline        = true,
   update_in_insert = false,
-  severity_sort = false,
+  severity_sort    = false,
+  float            = float_opts,
 }
