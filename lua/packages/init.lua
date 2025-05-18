ab3125fed9cb39c73af1e16b96cb7beed5c4451b
AnthonyK213 <tjjzqwh@outlook.com>
2025-05-18 19:35:49 +0800

    move kbd to dap-ui

--- lua/packages/init.lua
+++ lua/packages/init.lua
@@ -1053,6 +1053,7 @@ require("lazy").setup({
   },
   {
     "neovim/nvim-lspconfig",
+    -- NO VeryLazy!
     config = function() require("packages.nvim-lspconfig") end,
     dependencies = {
       {
@@ -1075,20 +1076,24 @@ require("lazy").setup({
   },
   {
     "mfussenegger/nvim-dap",
-    config = function() require("packages.nvim-dap") end,
-    keys = {
-      { "<F5>",       function() require("dap").continue() end },
-      { "<F10>",      function() require("dap").step_over() end },
-      { "<F23>",      function() require("dap").step_into() end },
-      { "<S-F11>",    function() require("dap").step_into() end },
-      { "<F47>",      function() require("dap").step_out() end },
-      { "<S-C-F11>",  function() require("dap").step_out() end },
-      { "<leader>db", function() require("dap").toggle_breakpoint() end },
-      { "<leader>dc", function() require("dap").clear_breakpoints() end },
-      { "<leader>dl", function() require("dap").run_last() end },
-      { "<leader>dr", function() require("dap").repl.toggle() end },
-      { "<leader>dt", function() require("dap").terminate() end },
+    cmd = {
+      "DapContinue",
+      "DapDisconnect",
+      "DapNew",
+      "DapTerminate",
+      "DapRestartFrame",
+      "DapStepInto",
+      "DapStepOut",
+      "DapStepOver",
+      "DapPause",
+      "DapEval",
+      "DapToggleRepl",
+      "DapClearBreakpoints",
+      "DapToggleBreakpoint",
+      "DapSetLogLevel",
+      "DapShowLog",
     },
+    config = function() require("packages.nvim-dap") end,
     dependencies = {
       {
         "jay-babu/mason-nvim-dap.nvim",
@@ -1109,12 +1114,26 @@ require("lazy").setup({
     config = function()
       local dap, dapui = require("dap"), require("dapui")
       dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
     end,
     keys = {
+      { "<F5>",       function() require("dap").continue() end },
+      { "<F10>",      function() require("dap").step_over() end },
+      { "<F23>",      function() require("dap").step_into() end },
+      { "<S-F11>",    function() require("dap").step_into() end },
+      { "<F47>",      function() require("dap").step_out() end },
+      { "<S-C-F11>",  function() require("dap").step_out() end },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end },
+      { "<leader>dc", function() require("dap").clear_breakpoints() end },
+      { "<leader>dl", function() require("dap").run_last() end },
+      { "<leader>dr", function() require("dap").repl.toggle() end },
+      { "<leader>dt", function() require("dap").terminate() end },
       { "<leader>dn", function() require("dapui").toggle() end },
       { "<leader>df", function() require("dapui").float_element() end },
-      { "<leader>dv", function() require("dapui").eval() end,         mode = "v" },
+      { "<leader>dv", function() require("dapui").eval() end,           mode = "v" },
     },
     dependencies = {
       "mfussenegger/nvim-dap",
