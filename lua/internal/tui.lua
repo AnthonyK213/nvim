70467aff7e30aa3e038d4a5a6e69aef772bfe854
Anthony <tjjzqwh@163.com>
2025-05-26 15:59:47 +0800

    default

--- lua/internal/tui.lua
+++ lua/internal/tui.lua
@@ -24,13 +24,18 @@ end
 ---@param exclude? string[]
 function M.set_color_scheme(exclude)
   vim.g._my_theme_switchable = false
+  local scheme = _G._my_core_opt.tui.scheme
   if M.use_nano() then
     M.nano_setup()
-  elseif exclude and vim.list_contains(exclude, _G._my_core_opt.tui.scheme) then
+  elseif exclude and vim.list_contains(exclude, scheme) then
     return
   else
-    if not pcall(vim.cmd.colorscheme, _G._my_core_opt.tui.scheme) then
-      vim.notify("Color scheme " .. tostring(_G._my_core_opt.tui.scheme) .. " was not found", vim.log.levels.WARN)
+    if scheme == "default" then
+      vim.g._my_theme_switchable = true
+      require("utility.theme").set_theme(_G._my_core_opt.tui.theme)
+    end
+    if not pcall(vim.cmd.colorscheme, scheme) then
+      vim.notify("Color scheme " .. scheme .. " was not found", vim.log.levels.WARN)
     end
   end
 end
